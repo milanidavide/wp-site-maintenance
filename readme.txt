fce43045ab1ccbdc5220ea96d131df8e68cd4d80
Davide Milani <milanidavide@milanidavide.com>
2024-03-24 20:47:30 +0100

    feat: v0.1.0

---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== WP Site Maintenance ===
+Tags: maintenance
+License: BSD-3-Clause license
+License URI: https://opensource.org/licenses/BSD-3-Clause
+
+Toggle maintenance mode and customize the maintenance screen.
+
+== Description ==
+
+WP Site Maintenance is a WordPress plugin that enables you to toggle maintenance mode and customize the maintenance screen.
+Administrators retain full access to the site during maintenance.
+
+Usage:
+- To activate maintenance mode, just set `WPSITEMAINT_IN_MAINTENANCE` to true in your wp-config.php file.
+- To customize the maintenance screen, simply add `wp-config/maintenance.php`.
+
+== Changelog ==
+
+= 0.1.0 =
+* Initial release.
+
+== Upgrade Notice ==
+
+= 0.1.0 =
+* This is the first release of the plugin.
---
+++ wp-site-maintenance.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Plugin Name: WP Site Maintenance
+ * Description: Toggle maintenance mode and customize the maintenance screen.
+ * Version: 0.1.0
+ * Author: Davide Milani
+ * Author URI: https://www.milanidavide.com/
+ * License: BSD-3-Clause license
+ * License URI: https://opensource.org/licenses/BSD-3-Clause
+ * Text Domain: wp-site-maintenance
+ * Domain Path: /languages
+ *
+ * @package WP Site Maintenance
+ */
+
+/**
+ * Handles maintenance mode for the WordPress site.
+ */
+function wpsitemaint_handle_maintenance_mode() {
+	if (
+		defined( 'WPSITEMAINT_IN_MAINTENANCE' )
+		&& WPSITEMAINT_IN_MAINTENANCE
+		&& ! current_user_can( 'administrator' )
+		&& 'wp-login.php' !== $GLOBALS['pagenow']
+	) {
+		$protocol = wp_get_server_protocol();
+		header( "$protocol 503 Service Unavailable", true, 503 );
+		header( 'Content-Type: text/html; charset=utf-8' );
+		header( 'Retry-After: 60' );
+
+		if ( file_exists( WP_CONTENT_DIR . '/maintenance.php' ) ) {
+			require_once WP_CONTENT_DIR . '/maintenance.php';
+		}
+
+		die();
+	}
+}
+add_action( 'wp_loaded', 'wpsitemaint_handle_maintenance_mode' );
